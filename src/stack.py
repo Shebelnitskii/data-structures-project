dbbfa15b01afc4794eff883de0bd060f70d3e724
Shebel <Shebelnitskiy@gmail.com>
2023-03-12 17:05:47 +0400

    fix class Stack and Node / add tests

--- src/stack.py
+++ src/stack.py
@@ -1,21 +1,23 @@
 class Node:
     """Класс для узла стека"""
 
-    def __init__(self, data, next_node):
+    def __init__(self, data = None, next_node = None):
         """
         Конструктор класса Node
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
-
+        self.data = data
+        self.next_node = next_node
 
 class Stack:
     """Класс для стека"""
 
     def __init__(self):
         """Конструктор класса Stack"""
-        pass
+        self.stack = []
+        self.top = 0
+        self.size = 0
 
     def push(self, data):
         """
@@ -23,7 +25,12 @@ class Stack:
 
         :param data: данные, которые будут добавлены на вершину стека
         """
-        pass
+        new_node = Node(data)
+        if self.top:
+            new_node.next_node = self.top
+        self.top = new_node
+        self.size += 1
+        self.stack.append(data)
 
     def pop(self):
         """
@@ -31,4 +38,12 @@ class Stack:
 
         :return: данные удаленного элемента
         """
-        pass
+        return self.stack.pop()
+
+    def top(self):
+        """
+        Метод для получения элемента с вершины стека и его возвращения
+
+        :return: данные удаленного элемента
+        """
+        return self.stack[:-1]
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,39 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+import pytest
+from src.stack import Node,Stack
+
+@pytest.fixture
+def node_1_none():
+    return Node(1,None)
+
+def test_node_1(node_1_none):
+    assert node_1_none.data == 1
+
+def test_node_none(node_1_none):
+    assert node_1_none.next_node == None
+
+@pytest.fixture
+def stack_zero():
+    return Stack()
+
+def test_stack_empty(stack_zero):
+    assert stack_zero.push('1') == None
+
+def test_stack_top(stack_zero):
+    stack_zero.push('1')
+    stack_zero.push('2')
+    stack_zero.push('3')
+    assert stack_zero.top.data == '3'
+
+def test_stack_next_node(stack_zero):
+    stack_zero.push('1')
+    stack_zero.push('2')
+    stack_zero.push('3')
+    assert stack_zero.top.next_node.data == '2'
+
+def test_stack_pop(stack_zero):
+    stack_zero.push('1')
+    stack_zero.push('2')
+    stack_zero.push('3')
+    stack_zero.pop()
+    assert stack_zero.pop() == '2'
