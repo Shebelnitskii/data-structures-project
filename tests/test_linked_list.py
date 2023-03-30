88fb4e15951c61ca7f274b558b5bb37230bdf185
Shebel <Shebelnitskiy@gmail.com>
2023-03-30 18:07:15 +0400

    add tests for class LinkedList methods to_list,get_data_by_id

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -44,3 +44,28 @@ def test_linked_list_str_non_empty():
     ll.insert_at_end({"id": 2})
     ll.insert_at_end({"id": 3})
     assert str(ll) == "{'id': 1} -> {'id': 2} -> {'id': 3} -> None"
+
+def test_to_list():
+    ll = LinkedList()
+    ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+    ll.insert_at_end({'id': 2, 'username': 'mik.roz'})
+    ll.insert_at_end({'id': 3, 'username': 'mosh_s'})
+    ll.insert_beginning({'id': 0, 'username': 'serebro'})
+    assert ll.to_list() == [{'id': 0, 'username': 'serebro'}, {'id': 1, 'username': 'lazzy508509'}, {'id': 2, 'username': 'mik.roz'}, {'id': 3, 'username': 'mosh_s'}]
+
+def test_get_data_by_id():
+    ll = LinkedList()
+    ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+    ll.insert_at_end({'id': 2, 'username': 'mik.roz'})
+    ll.insert_at_end({'id': 3, 'username': 'mosh_s'})
+    ll.insert_beginning({'id': 0, 'username': 'serebro'})
+    assert ll.get_data_by_id(3) == {'id': 3, 'username': 'mosh_s'}
+    assert ll.get_data_by_id(5) is None
+    assert ll.get_data_by_id('id_value') is None
+
+def test_get_data_by_error():
+    ll = LinkedList()
+    ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+    ll.insert_at_end('idusername')
+    ll.insert_at_end([1, 2, 3])
+    assert ll.get_data_by_id(3) == None
